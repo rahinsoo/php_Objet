ec8c6f57c3bb4782adc6444f76370b11ed43ef06
rahinsoo <rahinsoo@etik.com>
2025-11-07 15:47:18 +0100

    Modification plainte.php - Midification de la visibilité V.07

--- TP02-PDO/plainte.php
+++ TP02-PDO/plainte.php
@@ -2,46 +2,44 @@
 global $bdd;
 require_once "bdd.php";
 
-
-// supprimer une entité :
-if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['action']) && $_GET['action']=='suppression'){
-// réccupération de mon id
+// Supprimer une entité
+if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['action']) && $_GET['action']=='supprimer'){
     $id = $_GET['id'];
     $sql = "DELETE FROM plainte WHERE id = :id";
     $query = $bdd->prepare($sql);
-    $verif = $query-> execute([
-            'id'=> $id
-    ]);
+    $verif = $query->execute(['id' => $id]);
+
     if ($verif){
         header("Location: plainte.php");
-//        exit();
+        exit();
     }
+}
 
-    if(isset($_GET['id']) && !empty($_GET['id']) && !empty($_GET['visible'] && isset($_GET['action']) && $_GET['action']=='gerervisibilite')) {
-// réccupération de mon id
-        $visible = $_GET['visible'];
-        $id = $_GET['id'];
-        $sql = "UPDATE plainte SET visible = :visible WHERE id = :id";
-        $update = $bdd->prepare($sql);
-        $verif = $update->execute([
-                'id' => $id,
-                'visible' => $visible?0:1,
-        ]);
-        if ($verif) {
-            header("Location: plainte.php");
-//        exit();
-        }
+// Gérer la visibilité (SÉPARÉ, pas imbriqué !)
+if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['visible']) && isset($_GET['action']) && $_GET['action']=='gerervisibilite') {
+    $visible = $_GET['visible'];
+    $id = $_GET['id'];
+
+    // Inverser : si visible=1 alors mettre 0, si visible=0 alors mettre 1
+    $nouveauStatut = ($visible == 1) ? 0 : 1;
+
+    $sql = "UPDATE plainte SET visible = :visible WHERE id = :id";
+    $update = $bdd->prepare($sql);
+    $verif = $update->execute([
+            'id' => $id,
+            'visible' => $nouveauStatut,
+    ]);
+
+    if ($verif) {
+        header("Location: plainte.php");
+        exit();
     }
 }
-// && $_GET['status'] == modifier
-if(isset($_GET['id']) && !empty($_GET['id'])){
-    // ecrire le code pourmodifier le status d'une ligne
-}
+
+// Récupérer toutes les plaintes
 $sql = "SELECT * from plainte";
 $query = $bdd->query($sql);
 $plainte = $query->fetchAll();
-// && $_GET['status'] == supprimer
-
 ?>
 
     <!DOCTYPE html>
@@ -92,9 +90,15 @@ $plainte = $query->fetchAll();
                             <?php echo ($item['visible'] == 1) ? "<span class='btn btn-success'> visible</span>" : "<span class='btn btn-danger'> invisible</span>"; ?>
                     </th>
                     <th scope="col">
-                        <a href="plainte.php?id=<?php echo $item['id']; ?>=<?php $item['visible']; ?>"><span class='btn btn-danger'>Supprimer</span></a>
-                        <a href="edit_plainte.php?id=<?php echo $item['id']; ?>"><span class='btn btn-warning'>Editer</span></a>
-                        <a href="plainte.php?id=<?php echo $item['id']; ?>&visible<?php echo $item['visible']; ?>"><span class='btn btn-info'>Visible O/N</span></a>
+                        <a href="plainte.php?id=<?php echo $item['id']; ?>&action=supprimer">
+                            <span class='btn btn-danger'>Supprimer</span>
+                        </a>
+                        <a href="edit_plainte.php?id=<?php echo $item['id']; ?>">
+                            <span class='btn btn-warning'>Editer</span>
+                        </a>
+                        <a href="plainte.php?id=<?php echo $item['id']; ?>&visible=<?php echo $item['visible']; ?>&action=gerervisibilite">
+                            <span class='btn btn-info'>Visible O/N</span>
+                        </a>
                     </th>
                 </tr>
             <?php }
